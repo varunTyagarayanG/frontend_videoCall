f4532238e267609efdb9017ae23f23cbb182a077
varunTyagarayanG <varunmuralidhar9@gmail.com>
2025-03-10 01:07:52 +0530

    he

--- src/axios.js
+++ src/axios.js
@@ -1,35 +1,39 @@
-import axios from "axios"
-import store from './store/index'
+import axios from "axios";
+import store from "./store/index";
 
-console.log(import.meta.env)
+// Log API URL to confirm it's loaded correctly
+console.log("API URL:", import.meta.env.VITE_APP_API_URL);
 
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_APP_API_URL
-})
+    baseURL: import.meta.env.VITE_APP_API_URL,
+});
 
+// Add Authorization token if available
+instance.interceptors.request.use((config) => {
+    if (store.state.token) {
+        config.headers['Authorization'] = `Bearer ${store.state.token}`
+
+    }
+    return config;
+});
+
+// Request and Response Interceptors
 instance.interceptors.request.use(
     (config) => {
-        if (store.state.token) {
-            config.headers['Authorization'] = `Bearer ${store.state.token}`
-        }
-        return config
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-)
+);
 
-instance.interceptors.request.use(config => {
-    // app.$Progress.start();
-    return config
-}, (error) => {
-    // app.$Progress.fail();
-    return Promise.reject(error);
-})
-
-instance.interceptors.response.use(response => {
-    // app.$Progress.finish()
-    return response
-}, (error) => {
-    // app.$Progress.fail();
-    return Promise.reject(error);
-})
+instance.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default instance;
